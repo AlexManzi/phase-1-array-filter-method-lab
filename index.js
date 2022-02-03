47a578a4152d01d1113683a5672ae2719a531865
Alex Manzi <alex.manzi22@gmail.com>
2022-02-03 16:18:33 -0500

    commit tree 9fa688bd293881276ee9b93716da6b4fd7ec6787

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(array => array[0][0] === name[0][0])
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(drivers => drivers.name === string)
+}
